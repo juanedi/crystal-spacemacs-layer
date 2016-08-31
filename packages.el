fc82c32ded87f4887d52a08dfbeae6fdc66b88a1
Juan Edi <jedi@manas.com.ar>
2016-08-31 12:35:13 -0300

    Require crystal-mode package

--- packages.el
+++ packages.el
@@ -30,33 +30,11 @@
 ;;; Code:
 
 (defconst crystal-packages
-  '()
-  "The list of Lisp packages required by the crystal layer.
+  '(
+    (crystal-mode :location (recipe :fetcher github :repo "dotmilk/emacs-crystal-mode"))
+    )
+  )
 
-Each entry is either:
-
-1. A symbol, which is interpreted as a package to be installed, or
-
-2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
-    name of the package to be installed or loaded, and KEYS are
-    any number of keyword-value-pairs.
-
-    The following keys are accepted:
-
-    - :excluded (t or nil): Prevent the package from being loaded
-      if value is non-nil
-
-    - :location: Specify a custom installation location.
-      The following values are legal:
-
-      - The symbol `elpa' (default) means PACKAGE will be
-        installed using the Emacs package manager.
-
-      - The symbol `local' directs Spacemacs to load the file at
-        `./local/PACKAGE/PACKAGE.el'
-
-      - A list beginning with the symbol `recipe' is a melpa
-        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
-
+(defun crystal/init-crystal-mode ())
 
 ;;; packages.el ends here
