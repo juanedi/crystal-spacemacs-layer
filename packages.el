97ab02606e380de24a98b353bc8d9b647768951c
Juan Edi <jedi@manas.com.ar>
2016-08-31 12:46:24 -0300

    Automatic formatting

--- packages.el
+++ packages.el
@@ -35,6 +35,21 @@
     )
   )
 
+(defun crystal/format-file ()
+  (when (equal 'crystal-mode major-mode)
+    (let ((output-buffer "*crystal formatter*"))
+      (when (get-buffer output-buffer) (kill-buffer output-buffer))
+      (get-buffer-create output-buffer)
+      (let ((exit-status (call-process-shell-command (format "crystal tool format %s" buffer-file-name) nil output-buffer t)))
+        (if (eq 0 exit-status)
+            (revert-buffer t t)
+          (popwin:popup-buffer output-buffer))))))
+
 (defun crystal/init-crystal-mode ())
+
+(defun crystal/post-init-crystal-mode ()
+  ;; TODO: this should be optional
+  ;; TODO: register hook only for crystal files?
+  (add-hook 'after-save-hook #'crystal/format-file))
 
 ;;; packages.el ends here
